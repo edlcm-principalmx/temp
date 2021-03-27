7fa7b1e84e4800e1bfb3cf0ef911f59fef35b2c3
Ernesto de la Cruz <ernesto.delacruz.morales@gmail.com>
2021-03-26 23:13:15 -0500

    Codigo para Desencriptar AES128

---
+++ AESDecryptionTest/AESDecryptionTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AESDecryptionTest
+{
+    class Program
+    {
+        private static String key = "Procesar1*";
+
+        public string Decrypt(String encryptedText, String key)
+        {
+            var encryptedBytes = Convert.FromBase64String(encryptedText);
+            return Encoding.UTF8.GetString(Decrypt(encryptedBytes, GetRijndaelManaged(key)));
+        }
+
+        public byte[] Encrypt(byte[] plainBytes, RijndaelManaged rijndaelManaged)
+        {
+            return rijndaelManaged.CreateEncryptor()
+                .TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+        }
+
+        public byte[] Decrypt(byte[] encryptedData, RijndaelManaged rijndaelManaged)
+        {
+            return rijndaelManaged.CreateDecryptor()
+                .TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+        }
+
+        public RijndaelManaged GetRijndaelManaged(String secretKey)
+        {
+            var keyBytes = new byte[16];
+            var secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+            Array.Copy(secretKeyBytes, keyBytes, Math.Min(keyBytes.Length, secretKeyBytes.Length));
+            return new RijndaelManaged
+            {
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7,
+                KeySize = 128,
+                BlockSize = 128,
+                Key = keyBytes,
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            var instance = new Program();
+            String decryptedString = instance.Decrypt("kUDEc87LtpcTYUAiNlCcpNjxS2kszJVvP2/l3bCFH/o=", key);
+            Console.WriteLine(decryptedString);
+        }
+    }
+}
